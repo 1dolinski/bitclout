b4da7c58a0c6d1305f46a0d439faed9ce2c24ea2
Chris Dolinski <christopher.dolinski@gmail.com>
2021-04-16 13:57:00 -0400

    Updated

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a href="https://www.bitclout.com/u/bitcloutoffers">
+        <a href="https://www.bitclout.com/u/bitcloutoffers" style={{"color": "white"}}>
           @bitcloutoffers
         </a>
+        <br/>
+        <a href="https://airtable.com/shrCuDoIrnE4wNPyf" style={{"color": "white"}}>
+          Click here to add an offer
+        </a>
+        <br/>
 
-        <iframe class="airtable-embed" src="https://airtable.com/embed/shrOIHvUnSidfnZxP?backgroundColor=cyan&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
-        <iframe class="airtable-embed" src="https://airtable.com/embed/shrTx4wwPPXBdGqgn?backgroundColor=cyan&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
+
+
+        <iframe class="airtable-embed" src="https://airtable.com/embed/shrTx4wwPPXBdGqgn?backgroundColor=cyan&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style={{"background": "transparent", "border": "1px solid #ccc;"}}></iframe>
+        <br/>
+        <iframe class="airtable-embed" src="https://airtable.com/embed/shrOIHvUnSidfnZxP?backgroundColor=cyan&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style={{"background": "transparent", "border": "1px solid #ccc;"}}></iframe>
       </header>
     </div>
   );
