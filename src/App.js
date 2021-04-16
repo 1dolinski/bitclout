e6630e6748722e4195d9c1f43fd65db7af03c5b2
Chris Dolinski <christopher.dolinski@gmail.com>
2021-04-16 13:48:50 -0400

    Updated title, added airtables

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <a href="https://www.bitclout.com/u/bitcloutoffers">
           @bitcloutoffers
         </a>
+
+        <iframe class="airtable-embed" src="https://airtable.com/embed/shrOIHvUnSidfnZxP?backgroundColor=cyan&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
+        <iframe class="airtable-embed" src="https://airtable.com/embed/shrTx4wwPPXBdGqgn?backgroundColor=cyan&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
       </header>
     </div>
   );
