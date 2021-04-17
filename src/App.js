649e8163375e3ac0690b8b8cd8f31c21b7e2cd11
Chris Dolinski <christopher.dolinski@gmail.com>
2021-04-17 16:38:53 -0400

    Updaeted firebase

--- src/App.js
+++ src/App.js
@@ -15,37 +15,6 @@ import List from "./List.js";
 import Calendar from "./Calendar.js";
 import Bot from "./Bot.js";
 
-
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: 'keyR9jk3yd5Msam4M'}).base('appX3lOvCGm05jXmy');
-
-console.log(base);
-
-var titles = ['ok'];
-
-base('List').select({
-    // Selecting the first 3 records in @BitCloutOffers List:
-    maxRecords: 3,
-    view: "@BitCloutOffers List"
-}).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-
-    records.forEach(function(record) {
-        titles.concat(record.get('Title'));
-        console.log('Retrieved', record.get('Title'));
-    });
-
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
-
-}, function done(err) {
-    if (err) { console.error(err); return; }
-});
-
-
-
 export default function App() {
   return (
     <Router>
@@ -70,37 +39,3 @@ export default function App() {
     </Router>
   );
 }
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <a href="https://www.bitclout.com/u/bitcloutoffers" style={{"color": "white"}}>
-//           @bitcloutoffers
-//         </a>
-//         <br/>
-//         <a href="https://airtable.com/shrCuDoIrnE4wNPyf" style={{"color": "white"}}>
-//           Click here to add an offer
-//         </a>
-//         <br/>
-
-//         {titles[0]}
-
-//         <iframe className="airtable-embed" src="https://airtable.com/embed/shrTx4wwPPXBdGqgn?backgroundColor=cyan&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style={{"background": "transparent", "border": "1px solid #ccc"}}></iframe>
-//         <br/>
-//         <iframe className="airtable-embed" src="https://airtable.com/embed/shrOIHvUnSidfnZxP?backgroundColor=cyan&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style={{"background": "transparent", "border": "1px solid #ccc"}}></iframe>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,7 @@ const navigation = [
 
 export default function Home() {
   return (
+      <Fragment>
     <Popover className="relative bg-white overflow-hidden">
       {({ open }) => (
         <>
@@ -159,5 +160,34 @@ export default function Home() {
         </>
       )}
     </Popover>
+
+    <div className="bg-gray-50">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <span className="block">Do You Have an Offer or Event?</span>
+          <span className="block text-indigo-600">Add it to the List</span>
+        </h2>
+        <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+          <div className="inline-flex rounded-md shadow">
+            <a
+              href="https://airtable.com/shrCuDoIrnE4wNPyf" target="_blank"
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+            >
+              Add Offer or Event 
+            </a>
+          </div>
+          {/* <div className="ml-3 inline-flex rounded-md shadow">
+            <a
+              href="#"
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+            >
+              Learn more
+            </a>
+          </div> */}
+        </div>
+      </div>
+    </div>
+
+    </Fragment>
   )
 }
